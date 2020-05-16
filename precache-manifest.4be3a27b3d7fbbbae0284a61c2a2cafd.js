14a0004ad3a3ef316caf395fb7a547b2a47fec5f
Sarah Sweat <sarahsweat@gmail.com>
2020-05-16 17:58:11 -0400

    Updates

---
+++ precache-manifest.4be3a27b3d7fbbbae0284a61c2a2cafd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4714754ee87cc889ec139e596a2515",
+    "url": "/www.sarahsweat.com/index.html"
+  },
+  {
+    "revision": "133b16472a54ce190355",
+    "url": "/www.sarahsweat.com/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "509ad471a6b6e9dde260",
+    "url": "/www.sarahsweat.com/static/js/2.e0b8981c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/www.sarahsweat.com/static/js/2.e0b8981c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133b16472a54ce190355",
+    "url": "/www.sarahsweat.com/static/js/main.7bdc5d4d.chunk.js"
+  },
+  {
+    "revision": "ccb035eb67f68e714d27",
+    "url": "/www.sarahsweat.com/static/js/runtime-main.7b9bc1a1.js"
+  }
+]);
