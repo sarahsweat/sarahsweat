4e64dbdbc998525d84e07c386b3f4c8387bbc7bc
Sarah Sweat <sarahsweat@gmail.com>
2024-10-28 15:28:40 -0400

    prettier updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import Home from './pages/Home';
-import { SSTheme } from './ds/themes';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import Home from "./pages/Home";
+import { SSTheme } from "./ds/themes";
 
 const App = () => (
   <ThemeProvider theme={SSTheme}>
     <Home />
   </ThemeProvider>
-)
+);
 
 export default App;
